9d7dc14d394967369756ebc12823f75daa4b861d
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-10 14:44:11 +0530

    Update clonedb.py

--- VIPMUSIC/utils/database/clonedb.py
+++ VIPMUSIC/utils/database/clonedb.py
@@ -1,20 +1,21 @@
 from VIPMUSIC.core.mongo import mongodb, pymongodb
 from typing import Dict, List, Union
+from config import OWNER_ID
 
 cloneownerdb = mongodb.cloneownerdb
 clonebotdb = pymongodb.clonebotdb
 clonebotnamedb = mongodb.clonebotnamedb
 
-# clone bot owner
 async def save_clonebot_owner(bot_id, user_id):
     await cloneownerdb.insert_one({"bot_id": bot_id, "user_id": user_id})
+
 
 async def get_clonebot_owner(bot_id):
     result = await cloneownerdb.find_one({"bot_id": bot_id})
     if result:
         return result.get("user_id")
     else:
-        return False
+        return OWNER_ID
 
 async def save_clonebot_username(bot_id, user_name):
     await clonebotnamedb.insert_one({"bot_id": bot_id, "user_name": user_name})
@@ -24,4 +25,4 @@ async def get_clonebot_username(bot_id):
     if result:
         return result.get("user_name")
     else:
-        return False
+        return app.username
